a1da3ada9afcad70aecc3a35faa5be64191a3913
Bruno Couto <bruno.couto@bluewin.ch>
2017-03-16 17:43:13 +0100

    New Features: - Edit a visit - Remove a visit

--- src/pages/visit-detail/visit-detail.ts
+++ src/pages/visit-detail/visit-detail.ts
@@ -1,22 +1,64 @@
+import { FirebaseObjectObservable } from 'angularfire2';
+import { Validators } from '@angular/forms';
+import { TerritoryData } from './../../providers/territory-data';
+import { FormBuilder } from '@angular/forms';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the VisitDetail page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-visit-detail',
   templateUrl: 'visit-detail.html'
 })
 export class VisitDetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  public updateVisitForm;
+  public territoryId: string;
+  public residentId: string;
+  public visitId: string;
+  public visitDate: string;
+  public visitResult: string;
+  public visitRemarks: string;
+  public VisitDetail: FirebaseObjectObservable<any>;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad VisitDetailPage');
+  constructor(public navCtrl: NavController, public navParams: NavParams, public territoryData: TerritoryData, public formBuilder: FormBuilder) { 
+
+    this.territoryId = this.navParams.get('territoryId');
+    this.residentId = this.navParams.get('residentId');
+    this.visitId = this.navParams.get('visitId');
+
+    this.VisitDetail = this.territoryData.getVisit(this.territoryId, this.residentId, this.visitId);
+    this.VisitDetail.subscribe(snapshot => {
+      this.visitDate = snapshot.date
+      this.visitResult = snapshot.result
+      this.visitRemarks = snapshot.remarks
+    });
+
+    this.updateVisitForm = formBuilder.group({
+      date: [this.visitDate, Validators.required],
+      result: [this.visitResult, Validators.required],
+      remarks: [this.visitRemarks]
+    });
   }
+
+  updateVisit(){
+    if (!this.updateVisitForm.valid){
+      console.log(this.updateVisitForm.value);
+    } else {
+      this.territoryData.updateVisit(
+      this.territoryId,
+      this.residentId,
+      this.visitId,
+      this.updateVisitForm.value.date,
+      this.updateVisitForm.value.result,
+      this.updateVisitForm.value.remarks
+      ).then( () => {
+        this.navCtrl.pop();
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+
+
 
 }
--- src/pages/visit-list/visit-list.ts
+++ src/pages/visit-list/visit-list.ts
@@ -1,3 +1,5 @@
+import { AlertController } from 'ionic-angular';
+import { VisitDetailPage } from './../visit-detail/visit-detail';
 import { VisitCreatePage } from './../visit-create/visit-create';
 import { FirebaseListObservable } from 'angularfire2';
 import { ResidentCreatePage } from './../resident-create/resident-create';
@@ -16,7 +18,8 @@ export class VisitListPage {
   public residentId: any;
   public territoryId: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public territoryData: TerritoryData) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public territoryData: TerritoryData, public actionCtrl: ActionSheetController, public platform: Platform,
+              public alertCtrl: AlertController) {
 
     this.territoryId = this.navParams.get('territoryId');
     this.residentId = this.navParams.get('residentId');
@@ -29,5 +32,46 @@ export class VisitListPage {
     let territoryId = this.navParams.get('territoryId');
     let residentId = this.navParams.get('residentId');
     this.navCtrl.push(VisitCreatePage, {territoryId, residentId});
+  }
+
+  // Show more resident option
+  moreVisitOption(visitId: string) {
+    let action = this.actionCtrl.create({
+      buttons: [
+        {
+          text: 'Editar',
+          icon: !this.platform.is('ios') ? 'create' : null,
+          handler: () => {
+            this.navCtrl.push(VisitDetailPage, { territoryId: this.territoryId, residentId: this.residentId, visitId: visitId });
+          }
+        },
+        {
+          text: 'Remover',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            let alert = this.alertCtrl.create({
+              title: 'Remover a visita !',
+              subTitle: 'Desejas remover esta visita ?',
+              buttons: [
+                {
+                  text: 'Remover',
+                  role: 'destructive',
+                  handler: () => {
+                    this.territoryData.removeVisit(this.territoryId, this.residentId, visitId);
+                  }
+                },
+                {
+                  text: 'Cancelar',
+                  role: 'cancel'
+                }
+              ]
+            });
+            alert.present();
+          }
+        }
+      ]
+    });
+    action.present();
   }
 }
